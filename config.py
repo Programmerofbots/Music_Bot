4088f59ba81bb9e73aeb14fc5edf93c82f7c43b8
HACKER OP <149420286+Programmerofbots@users.noreply.github.com>
2024-01-18 11:55:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 1000))
 LOGGER_ID = int(getenv("LOGGER_ID", None))
 
 # Get this value from @FallenxBot on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", 6402115032))
+OWNER_ID = int(getenv("OWNER_ID", 2081040115))
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
@@ -39,8 +39,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Stylish_Bio_Telegram_xyz")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/all_exam_quiz_new")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/O_P_Hacker")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Princes_log_support_group")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
@@ -78,13 +78,13 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://telegra.ph/file/d1060a096af511f0907ea.jpg"
+    "START_IMG_URL", "https://te.legra.ph/file/52792e7acc085c69eeb14.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://telegra.ph/file/d1060a096af511f0907ea.jpg"
+    "PING_IMG_URL", "https://te.legra.ph/file/52792e7acc085c69eeb14.jpg"
 )
 PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
-STATS_IMG_URL = "https://te.legra.ph/file/e906c2def5afe8a9b9120.jpg"
+STATS_IMG_URL = "https://te.legra.ph/file/52792e7acc085c69eeb14.jpg"
 TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
 TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
 STREAM_IMG_URL = "https://te.legra.ph/file/bd995b032b6bd263e2cc9.jpg"
@@ -112,12 +112,12 @@ if SUPPORT_CHANNEL:
 if PING_IMG_URL:
     if PING_IMG_URL != "assets/Ping.jpeg":
         if not re.match("(?:http|https)://", PING_IMG_URL):
-            PING_IMG_URL = "https://telegra.ph/file/d1060a096af511f0907ea.jpg"
+            PING_IMG_URL = "https://te.legra.ph/file/52792e7acc085c69eeb14.jpg"
 
 if START_IMG_URL:
     if START_IMG_URL != "assets/Ping.jpeg":
         if not re.match("(?:http|https)://", START_IMG_URL):
-            START_IMG_URL = "https://telegra.ph/file/d1060a096af511f0907ea.jpg"
+            START_IMG_URL = "https://te.legra.ph/file/52792e7acc085c69eeb14.jpg"
             
 if SUPPORT_CHAT:
     if not re.match("(?:http|https)://", SUPPORT_CHAT):
